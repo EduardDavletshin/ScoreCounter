4fc519833f8a07feb065024d7d5dc0659dc56c5a
EduardDavletshin <eduard.davletshin@flatstack.com>
2016-12-06 18:48:17 +0300

    covert listview to recyclerview with cardview

--- app/src/main/java/com/example/eddy/basketballcounter/SecondaryActivity.java
+++ app/src/main/java/com/example/eddy/basketballcounter/SecondaryActivity.java
@@ -2,10 +2,11 @@ package com.example.eddy.basketballcounter;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
+import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -15,34 +16,31 @@ import static com.example.eddy.basketballcounter.R.array.colorslist;
 public class SecondaryActivity extends AppCompatActivity {
 
     int i = 0;
-    String[] colorsList;
+    String[] colorNames;
+    int[] colors;
+    RecyclerView rv;
+    RecyclerView.Adapter rvAdapter;
+    RecyclerView.LayoutManager rvLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.secondary_activity);
         setTitle("Background setter");
-        colorsList = getBaseContext().getResources().getStringArray(color_names);
-        final ListView listView = (ListView) findViewById(R.id.color_names);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, colorsList);
-        listView.setAdapter(adapter);
-        Button showList = (Button) findViewById(R.id.show_list);
-        showList.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (listView.getVisibility() == View.GONE) {
-                    listView.setVisibility(View.VISIBLE);
-                } else listView.setVisibility(View.GONE);
-            }
-        });
+        colorNames = getBaseContext().getResources().getStringArray(color_names);
+        rv = (RecyclerView) findViewById(R.id.rv);
+        rv.setHasFixedSize(true);
+        rvLayoutManager = new LinearLayoutManager(this);
+        rv.setLayoutManager(rvLayoutManager);
+        rvAdapter = new rvAdapter(colorNames);
+        rv.setAdapter(rvAdapter);
     }
 
-
     public void onClickChangeBackground(View view) {
-        int[] colors = getBaseContext().getResources().getIntArray(colorslist);
         String[] color_names = getBaseContext().getResources().getStringArray(R.array.color_names);
-        final RelativeLayout layout = (RelativeLayout) findViewById(R.id.activity_main2);
-        final TextView textView = (TextView) findViewById(R.id.color_name);
+        colors = getBaseContext().getResources().getIntArray(colorslist);
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.activity_main2);
+        TextView textView = (TextView) findViewById(R.id.color_name);
         if (i != colors.length) {
             layout.setBackgroundColor(colors[i]);
             textView.setText(color_names[i]);
@@ -51,6 +49,40 @@ public class SecondaryActivity extends AppCompatActivity {
             layout.setBackgroundColor(colors[0]);
             textView.setText(color_names[0]);
             i = 1;
+        }
+    }
+
+    public class rvAdapter extends RecyclerView.Adapter<rvAdapter.ViewHolder> {
+        public rvAdapter(String[] data) {
+            colorNames = data;
+        }
+
+        @Override
+        public rvAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+            View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.colors_card_view, viewGroup, false);
+            v.setPadding(0, 20, 0, 20);
+//            v.setBackgroundColor(colors[0]);
+            ViewHolder viewHolder = new ViewHolder(v);
+            return viewHolder;
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public TextView color;
+            public ViewHolder(View v) {
+                super(v);
+                color = (TextView) itemView.findViewById(R.id.color_name);
+
+            }
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder viewHolder, int position) {
+            viewHolder.color.setText(colorNames[position]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return colorNames.length;
         }
     }
 }
